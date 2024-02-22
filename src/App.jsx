e02fbae3efd8f54b55a47c9c9bf625f1bc8c8ef8
Jose Albornoz <jose.albornoz@outlook.cl>
2024-02-21 22:28:24 -0300

    Avances del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { UserContext } from "./providers/UserProvider";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const { token } = useContext(UserContext);
 
   return (
     <>
-      Proyecto en blanco
+      <Navbar />
     </>
   )
 }
