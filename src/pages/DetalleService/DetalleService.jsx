4396d0138ffefe1df222fce9dd7e15d394f81f5c
Pablo <emilio7b@gmail.com>
2024-02-24 22:00:25 -0300

    avance detalle de servicio

--- src/pages/DetalleService/DetalleService.jsx
+++ src/pages/DetalleService/DetalleService.jsx
@@ -1,5 +1,22 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { Button, Card } from 'react-bootstrap';
+
 export default function DetalleService() {
   return (
-    <div>DetalleService</div>
+    <div>
+      <Card border="light" className="text-center">
+      <Card.Header>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="dark">Go somewhere</Button>
+      </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
+    </div>
   )
 }
+
