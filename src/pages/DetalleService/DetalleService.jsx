6bf3647ad3bd6599d9341cf3a087cc8f9637533a
Pablo <emilio7b@gmail.com>
2024-02-24 22:02:01 -0300

    avance detalle de servicio

--- src/pages/DetalleService/DetalleService.jsx
+++ src/pages/DetalleService/DetalleService.jsx
@@ -3,9 +3,12 @@ import { useParams } from 'react-router-dom'
 import { Button, Card } from 'react-bootstrap';
 
 export default function DetalleService() {
+
+  const {id}= useParams();
+
   return (
     <div>
-      <Card border="light" className="text-center">
+      <Card key={1} border="light" className="text-center">
       <Card.Header>Featured</Card.Header>
       <Card.Body>
         <Card.Title>Special title treatment</Card.Title>
